9604a721426545e8bdbe7756267f9b5f23f136dd
GerFr <140012598+GerFr@users.noreply.github.com>
2024-03-29 20:43:40 +0100

    add email service and markdown format

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import markdown2
 
 URL_FIZZ  = "https://www.the-fizz.com/search/?searchcriteria=BUILDING:THE_FIZZ_HAMBURG_STUDENTS;AREA:HAMBURG" 
 URL_ILIVE = "https://www.urban-living-hamburg.de/mieten"
@@ -18,13 +19,43 @@ sender_email = os.getenv("SENDER")
 password = os.getenv("APP_PASSWORD")
 recipient_email = os.getenv("RECEIVER")
 
-text = f"{fizz_data[1], ilive_data[1]}"
+text = f"""
+## FIZZ Apartment
 
+[website]({URL_FIZZ})
+
+available: 
+
+{fizz_data[0]}
+
+data:
+
+{fizz_data[1]}
+
+
+## Urban-Living Apartment
+
+[website]({URL_ILIVE})
+
+available: 
+
+{ilive_data[0]}
+
+data:
+
+{ilive_data[1]}
+"""
+
+html_text = markdown2.markdown(text)
 msg = MIMEMultipart('alternative')
 msg['From'] = sender_email
 msg['To'] = recipient_email
 msg['Subject'] = "Data"
-msg.attach(MIMEText(text, 'plain'))
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html_text, 'html')
+msg.attach(part1)
+msg.attach(part2)
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()  
