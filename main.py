2557b2fd79633390bdd47bdf51bac71c8a143465
GerFr <140012598+GerFr@users.noreply.github.com>
2024-03-29 20:10:43 +0100

    add email sending

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from fizz import rooms_available as fizz_available
 from ilive import rooms_available as ilive_available
 
+import os
+from dotenv import load_dotenv
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 URL_FIZZ  = "https://www.the-fizz.com/search/?searchcriteria=BUILDING:THE_FIZZ_HAMBURG_STUDENTS;AREA:HAMBURG" 
 URL_ILIVE = "https://www.urban-living-hamburg.de/mieten"
 
-print(fizz_available(URL_FIZZ))
-print(ilive_available(URL_ILIVE))
+fizz_data = fizz_available(URL_FIZZ)
+ilive_data = ilive_available(URL_ILIVE)
+
+load_dotenv()
+sender_email = os.getenv("SENDER")
+password = os.getenv("PASSWORD")
+recipient_email = os.getenv("RECEIVER")
+
+text = f"{fizz_data[1], ilive_data[1]}"
+
+msg = MIMEMultipart('alternative')
+msg['From'] = sender_email
+msg['To'] = recipient_email
+msg['Subject'] = "Data"
+msg.attach(MIMEText(text, 'plain'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()  
+server.login(sender_email, password)
+server.sendmail(sender_email, recipient_email, msg.as_string())
+server.quit()
